bdad6f3b7446be7053b761c62f3211dc30634cf5
Feddas <feddas00@gmail.com>
2023-05-21 22:56:56 -0700

    A fresh clone of the project no longer throws exceptions in Unity2021.3.2f1. It was failing due to compatibility errors with latest version of IDE integration packages. com.unity.ide.visualstudio & rider. Ability to use earlier version of packages was removed from Unity. Added 3rd party tool to re-enable easily using older versions of package.

---
+++ Assets/FixPackageManagerSeeAllVersions/Editor/FixPackageManagerSeeAllVersions.cs
@@ -0,0 +1,36 @@
+// Cloned from https://github.com/forestrf/FixPackageManagerSeeAllVersions Details on why it's need at https://forum.unity.com/threads/see-all-versions-command-was-removed.1008619/
+
+#if UNITY_2021_1_OR_NEWER
+using Harmony;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class FixPackageManagerSeeAllVersions {
+	static HarmonyInstance harmony;
+
+	[InitializeOnLoadMethod]
+	static void Init() {
+		Patch();
+	}
+
+	static void Patch() {
+		MethodInfo Method_IsDeveloperBuild = typeof(Unsupported).GetMethod("IsDeveloperBuild", BindingFlags.Static | BindingFlags.Public);
+
+		if (Method_IsDeveloperBuild == null) {
+			Debug.LogWarning("Can't find the method Unsupported.IsDeveloperBuild(). Patching won't be done.");
+			return;
+		}
+
+		if (harmony == null) harmony = HarmonyInstance.Create("com.Nukefist.Ashkatchap.FixPackageManagerSeeAllVersions");
+		else harmony.UnpatchAll();
+
+		harmony.Patch(Method_IsDeveloperBuild, prefix: new HarmonyMethod(typeof(FixPackageManagerSeeAllVersions), "ReturnTrue"));
+	}
+
+	static bool ReturnTrue(ref bool __result) {
+		__result = true;
+		return false;
+	}
+}
+#endif
